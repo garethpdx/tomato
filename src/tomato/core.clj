9cd7d1bc9bf6ea70d3bd1264d0e8a1cc132577f4
Gareth M <gareth@crumpet>
2016-01-21 21:27:59 -0800

    Add a text status indicator displaying either Idle, Started, or Finish Up!

--- src/tomato/core.clj
+++ src/tomato/core.clj
@@ -7,40 +7,46 @@
 (use 'clojure.repl)
 (native!)
 
+(defn tomato? [tomato] (before? (now) tomato))
 
+(def status (label :text "Idle"  :h-text-position :right))
 
 (defn pom-done[]
   (sh "notify-send" (clojure.string/join
                      ["--icon=" (System/getProperty "user.dir") "/resources/tomato.png"])
       "Buzz!")
-  )
-
-(defn tomato? [tomato] (before? (now) tomato)
+  (text! status "Idle")
   )
 
 (defn wait[]
-  (def tomato (plus (now) (minutes 25)) )
-  (while (tomato? tomato)
-    (do (Thread/sleep 1000)))
-  (pom-done)
+  (text! status "Started")
+  (timer (fn [_](text! status "Finish up!")) :initial-delay (* 23 60 1000) :repeats? false)  
+  (timer (fn [_](pom-done)) :initial-delay (* 25 60 1000) :repeats? false)
   )
 
+(def c
+  (grid-panel
+   :hgap 10
+   :vgap 10
+   :columns 1
+   :items [(left-right-split
+           (label
+            :text "Click to Begin"
+            :icon (clojure.string/join ["file://"
+                                        (System/getProperty "user.dir")
+                                        "/resources/tomato.png"])
+            :listen [:mouse-clicked (fn [e] (wait))]
+            )
+           status
+           :divider-size 20)]
+   )
+  )
+(def f (frame :title "Tomato Timer"
+              :width 450
+              :height 200
+              :on-close :exit))
 (defn -main
   [& args]
-  (def f (frame :title "Tomato Timer"
-                :width 350
-                :height 200))
   (-> f show!)
-  (def c
-    (grid-panel
-     :hgap 10
-     :vgap 10
-     :columns 1
-     :items [(label
-              :text "Click to Begin"
-              :icon (clojure.string/join ["file://"
-                                          (System/getProperty "user.dir")
-                                          "/resources/tomato.png"])
-              :listen [:mouse-clicked (fn [e] (wait))])
-             ]))
-  (config! f :content c))
+  (config! f :content c)
+  )
